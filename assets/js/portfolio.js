92f4b1fced32f7622cd991b56f9f308b775bb33a
Philip Ross <philiplross@gmail.com>
2018-12-10 21:28:46 -0800

    v1.3

---
+++ assets/js/portfolio.js
@@ -0,0 +1,6 @@
+// $("#submitBtn").click(this).alert("Thank you for contacting us, we will be with you shortly.")
+$(document).ready(function(){
+    $("#submitBtn").click(function(){
+        alert("Thank you for contacting us, we will be with you shortly.");
+    });
+});
