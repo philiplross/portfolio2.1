47c0a1a7181ee233b63fcf61c7d1bd91606ea21f
Philip Ross <philiplross@gmail.com>
2018-12-13 01:41:16 -0800

    the big 2.0

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,4 +1,7 @@
 // $("#submitBtn").click(this).alert("Thank you for contacting us, we will be with you shortly.")
+
+
+
 $(document).ready(function(){
     $("#submitBtn").click(function(){
         alert("Thank you for contacting us, we will be with you shortly.");
